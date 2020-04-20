57fae79e405d13d05c3b53581b029458bf235ba3
jacobproffer <jacobhardingproffer@gmail.com>
2020-04-20 12:28:33 -0400

    Work on animations

--- themes/blackwater/assets/js/app.js
+++ themes/blackwater/assets/js/app.js
@@ -2,9 +2,11 @@ var mainHeader = document.querySelector(".main-header");
 var headerHeight = mainHeader.offsetHeight;
 var mobileNavButton = document.querySelector(".main-header__mobile-nav-button");
 var mobileNavigation = document.querySelector(".main-header__mobile-nav");
+var aboutGrid = document.querySelector(".home-about__grid");
+var mediaGrid = document.querySelector(".home-media__grid");
+var lazyImg = document.querySelectorAll("img");
 
 var headroom = new Headroom(mainHeader, {
-
   offset: 0,
   tolerance: 0,
   classes: {
@@ -34,6 +36,52 @@ mobileNavButton.addEventListener("click", function() {
   this.classList.toggle("nav-open");
 });
 
-var lazyLoadInstance = new LazyLoad({
-  elements_selector: ".lazy"
-});
+var controller = new ScrollMagic.Controller();
+
+if (lazyImg) {
+  lazyImg.forEach(function (el) {
+    var LazyScene = new ScrollMagic.Scene({
+      triggerElement: el,
+      triggerHook: 1,
+      offset: -300,
+      duration: 0,
+      reverse: false
+    })
+      .on('enter', function () {
+        if (el.getAttribute('data-src')) {
+          el.src = el.getAttribute('data-src')
+        }
+        if (el.getAttribute('data-srcset')) {
+          el.setAttribute('srcset', el.getAttribute('data-srcset'))
+        }
+      })
+      .addTo(controller)
+  });
+}
+
+if (aboutGrid) {
+  var aboutTL = gsap.timeline();
+
+  aboutTL
+    .from(".home-about__image-left img", {duration: 0.5, opacity: 0, stagger: 0.25, x: -200})
+    .from(".home-about__image-right img", {duration: 0.5, opacity: 0, stagger: 0.25, x: 200});
+
+  var aboutTrigger = new ScrollMagic.Scene({
+    triggerElement: aboutGrid,
+    triggerHook: 0.4,
+    duration: 0,
+  }).setTween(aboutTL).addTo(controller);
+}
+
+if (mediaGrid) {
+  var imageTL = gsap.timeline();
+
+  imageTL.from(".animate-image", {duration: 0.5, opacity: 0, stagger: 0.25});
+
+  var imageTrigger = new ScrollMagic.Scene({
+    triggerElement: mediaGrid,
+    triggerHook: 0.4,
+    duration: 0,
+    offset: 50,
+  }).setTween(imageTL).addTo(controller);
+}
